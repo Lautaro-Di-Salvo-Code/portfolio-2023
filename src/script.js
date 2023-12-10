9dae70f67ab89d1c53a1fda33dfd59688e304eae
primer push <lautarodisalvo5@gmail.com>
2023-12-10 16:47:27 -0300

    mobile-header-view

--- src/script.js
+++ src/script.js
@@ -1,62 +1,51 @@
-let slider = ( ) => {
-    let sig = document.querySelector(".next")
-    let atras = document.querySelector(".back")
-    let nietos = document.querySelectorAll(".nieto-slider")
-    // padre = document.querySelectorAll(".hijo-slider")
-    
-let i = 0
 
-document.addEventListener("click", e=>{
-    // si se hace click en atras
-    if(e.target === atras){
-        // remueve del indice 0, ya que es lo primero que está asignado como valor en 
-        // la variable i
-        nietos[i].classList.remove("active")
-        // se resta uno a la i y queda en numero negativo
-        i--;
-        
-        console.log(i)
-        // como en un array es posible que en un array antes del 0, haya un -1
-        //  se valida
-        if(i < 0){
-            // se le asigna a i el numero de la longitud del array entera restada en menos 1
-            i = nietos.length - 1
+
+let buttonNavbar =()=>{
+
+
+
+    let enlace = document.getElementById("barra-nav-link")
+    let buttonOpen = document.querySelector("size-button-navbar-open")
+    let buttonClose = document.querySelector("size-button-navbar-close")
+
+
+    document.addEventListener("click" , e=>{
+
+        if(e.target.matches(".size-button-navbar-open ")){
+            e.target.style.display = "none"
+            document.querySelector(".size-button-navbar-close").style.display = "block"
+            
+        }else if(".size-button-navbar-close"){
+            document.querySelector(".size-button-navbar-close").style.display = "none"
+            document.querySelector(".size-button-navbar-open").style.display = "block"
         }
-        // y a la posicion nueva, en su indice que seria dentro del array, un elemento html
-        // se le agrega en la clase, active
-        nietos[i].classList.add("active")
-    }
-    // si se hace click en boton siguiente
-    if(e.target === sig ){
-        // remueve active al elemento en  el primer indice, con i valiendo 0
-        nietos[i].classList.remove("active")
-        // se le suma 1, y avanza de posicion por el array que es el conjunto de hijos
-        i++;
         
-        // si i es mayor o igual a la longitud del array, suponiendo que
-        // hay indice de más, mas allá del limite del array
-        if(i >= nietos.length){
-            // devolvemos a la posicion cero del array, osea el primer hijo
-            i = 0
-            console.log(nietos)
-            console.log(i)
+        if(e.target.matches(".size-button-navbar-open ")){
+            document.querySelector(".nav-positioning-mob").classList.add("active")
+        }else if(".size-button-navbar-close"){
+            document.querySelector(".nav-positioning-mob").classList.remove("active")
+            
+        }   
+        
+        if(e.target.matches(".nav-positioning-mob")){
+            return null
+            
         }
-        // y le agregamos active a ese ultimo indice
-            nietos[i].classList.add("active")
+        
+        if(e.target.matches(enlace)){
+            document.querySelector(".nav-positioning-mob").classList.remove("active")
+            buttonClose.style.display = "none"
+            // buttonOpen.style.display = "block"
+            
+        }
+    })
 
 
-    }
-    
-})
+
+}
 
 
-   
-} 
-// slider()
 document.addEventListener("DOMContentLoaded" , e=>{
-    slider(".nieto-slider")
 
-
-
+    buttonNavbar(".size-button-navbar-open")
 })
-
